85a0f973d2492642b7736d763a8b50b66d15b8b4
SuryodayBasak <suryodaybasak@gmail.com>
2017-09-11 12:20:25 +0530

    Able to retrieve dataset from PHL's website

--- DataAcquisition/gettingData.py
+++ DataAcquisition/gettingData.py
@@ -1,1 +1,17 @@
- 
+import requests
+import sys
+
+url = 'http://www.hpcf.upr.edu/~abel/phl/phl_hec_all_confirmed.csv.zip'  
+print("Attempting to download dataset.")
+r = requests.get(url)
+
+try:
+	with open("dataset.zip", "wb") as code:
+		code.write(r.content)
+	print("Data retrieval successful.")
+
+except:
+	print("Error in downloading file!")
+	sys.exit(0)
+	
+
