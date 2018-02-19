05fc73a769c35c5c261d707f503ba5096235d66d
SuryodayBasak <suryodaybasak@gmail.com>
2018-02-19 11:58:08 +0530

    completed movie making

--- MLAnalysis/sga-cdhpf-conv-comb-movie.py
+++ MLAnalysis/sga-cdhpf-conv-comb-movie.py
@@ -2,7 +2,9 @@ import numpy as np
 from random import randint
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+#import matplotlib.animation as animation
+#from matplotlib import style
+#style.use('fivethirtyeight')
 
 def sga(rate, a1, a2, F1, F2, UL):
     LL = 0.001
@@ -28,12 +30,10 @@ def sga(rate, a1, a2, F1, F2, UL):
     cdhsx = (F1**a1)*(F2**a2)
     return a1, a2, rate, cdhsx
 
-R = 5
-D = 5
-T = 5
-#T = 396.5/288
-#T = 1833.5
-V = 5
+R = 1.99
+D = 1.03
+T = 1833.5/288
+V = 2.02
 
 a = 0.01
 b = 0.01
@@ -50,10 +50,11 @@ gamma = []
 delta = []
 cdhs_i = []
 cdhs_e = []
-cdhs = []
+#cdhs = []
 
 fig = plt.figure()
-plt.title('Gradient ascent towards maxima of the CDHS score of 55 Cnc e')
+
+#plt.title('Gradient ascent towards maxima of the CDHS score of 55 Cnc e')
 font = {'family' : 'normal',
         'weight' : 'bold',
         'size'   : 8}
@@ -65,7 +66,7 @@ ax1.set_ylabel(r'$\beta$' + '  (beta)')
 ax1.set_zlabel('CDHS_i')
 ax1.set_xlim3d(0, 1)
 ax1.set_ylim3d(0, 1)
-ax1.set_zlim3d(0, 5)
+ax1.set_zlim3d(0, 2)
 
 ax2 = fig.add_subplot(122, projection = '3d')
 ax2.set_xlabel(r'$\gamma$' + '  (gamma)')
@@ -73,13 +74,12 @@ ax2.set_ylabel(r'$\delta$' + '  (delta)')
 ax2.set_zlabel('CDHS_e')
 ax2.set_xlim3d(0, 1)
 ax2.set_ylim3d(0, 1)
-ax2.set_zlim3d(0, 5)
-#fig.subplots_adjust(left=-0.5, right=-0.9, top=-0.9, bottom=-0.5)
+ax2.set_zlim3d(0, 2)
 
 ll = 0
 img_index = 0
-for i in range(0, 19):
-    ll = ll + 0.05
+for i in range(0, 20):
+
     ul = ll + 0.05
     a1, b1, r11, c11 = sga(r1, a, b, R, D, ul)
     g1, d1, r21, c21 = sga(r2, g, d, R, D, ul)
@@ -91,13 +91,16 @@ for i in range(0, 19):
 
     cdhs_i.append(c1)
     cdhs_e.append(c2)
-    cdhs.append((0.5*c11)*(0.5*c21))
+    #cdhs.append((0.5*c11)*(0.5*c21))
+    cdhs = (0.5*c11) + (0.5*c21)
     print(a, b, g, d, c1, c2)
 
-    ax1.plot([a, a1], [b, b1], [c1, c11], c='r')
-    ax2.plot([g, g1], [d, d1], [c2, c21], c='r')
-    ax1.scatter(a1, b1, c11, c='r', marker = '^')
-    ax2.scatter(g1, d1, c21, c='r', marker = '^')
+    plt.title('Gradient Ascent Towards Maxima of CDHS for 55 Cnc e\nCDHS = '
+                    + str(cdhs), loc='right')
+    ax1.plot([a, a1], [b, b1], [c1, c11], c='b')
+    ax2.plot([g, g1], [d, d1], [c2, c21], c='b')
+    ax1.scatter(a1, b1, c11, c='r', marker = 'o', linewidths=0.01)
+    ax2.scatter(g1, d1, c21, c='r', marker = 'o', linewidths=0.01)
 
     a = a1
     b = b1
@@ -105,11 +108,13 @@ for i in range(0, 19):
     d = d1
     c1 = c11
     c2 = c21
-    r1 = r11
-    r2 = r21
+    #r1 = r11
+    #r2 = r21
 
     plt.savefig('movie-frames/' + str(img_index)+'.png')
+    #plt.show()
     img_index = img_index + 1
+    ll = ll + 0.05
 print('---')
 
 plt.show()
